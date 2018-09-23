bfb0aceadc78c4da22d51e3556668df1862bb77c
yhlai <yhlaikk@gmail.com>
2018-09-23 12:38:52 +0800

    Talent HomeAutomation get Theme

--- mobile/src/main/java/org/openhab/habdroid/ui/TalentHomeAutomationActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/TalentHomeAutomationActivity.java
@@ -3,18 +3,27 @@ package org.openhab.habdroid.ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import org.openhab.habdroid.R;
+import org.openhab.habdroid.util.Util;
 
-public class TalentHomeAutomationActivity extends AppCompatActivity {
+public class TalentHomeAutomationActivity extends AppCompatActivity implements
+        FragmentManager.OnBackStackChangedListener {
+    private final static String TAG = TalentHomeAutomationActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Util.setActivityTheme(this);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_talent_home_automation);
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,4 +37,8 @@ public class TalentHomeAutomationActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackStackChanged() {
+
+    }
 }

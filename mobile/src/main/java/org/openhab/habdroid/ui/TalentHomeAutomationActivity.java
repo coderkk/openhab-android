f7c85e13e497359d9630f10dbe9731ab134d3155
yhlai <yhlaikk@gmail.com>
2018-09-23 15:41:43 +0800

    From TalentHomeAutomationActivity back to Home

--- mobile/src/main/java/org/openhab/habdroid/ui/TalentHomeAutomationActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/TalentHomeAutomationActivity.java
@@ -1,11 +1,13 @@
 package org.openhab.habdroid.ui;
 
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.openhab.habdroid.R;
@@ -26,6 +28,11 @@ public class TalentHomeAutomationActivity extends AppCompatActivity implements
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Toolbar toolbar = findViewById(R.id.openhab_toolbar);
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,10 +42,36 @@ public class TalentHomeAutomationActivity extends AppCompatActivity implements
                         .setAction("Action", null).show();
             }
         });
+
+        updateTitle();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        Util.overridePendingTransition(this, true);
     }
 
     @Override
     public void onBackStackChanged() {
+        updateTitle();
+    }
 
+    private void updateTitle() {
+        FragmentManager fm = getSupportFragmentManager();
+        int count = fm.getBackStackEntryCount();
+        @StringRes int titleResId = count > 0
+                ? fm.getBackStackEntryAt(count - 1).getBreadCrumbTitleRes()
+                : R.string.homeauto_title;
+        setTitle(titleResId);
     }
 }
